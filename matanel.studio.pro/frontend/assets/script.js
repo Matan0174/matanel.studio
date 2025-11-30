4d533c7558b65a4d97c3a185ec6517b7a569c9ec
Matan0174 <nmbtknni77@gmail.com>
2025-11-30 15:42:15 +0200

    Initial commit - matanel.studio website #16

--- matanel.studio.pro/frontend/assets/script.js
+++ matanel.studio.pro/frontend/assets/script.js
@@ -5,7 +5,7 @@ document.getElementById("contactForm")?.addEventListener("submit", async functio
     const formData = new FormData(this);
 
     try {
-        const response = await fetch("https://matanel-studio-79613215467.us-central1.run.app/contact", {
+        const response = await fetch("https://matanel-studio-backend-79613215467.us-central1.run.app/contact", {
             method: "POST",
             body: formData
         });
